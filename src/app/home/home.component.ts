b4452ca28b6c6fd878c8fc54f45a64e1a8515bb0
Turkana Mammadova <mmmmadova.turkana@gmail.com>
2023-07-10 07:38:40 +0400

    New corrections

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-home',
@@ -17,9 +18,16 @@ export class HomeComponent {
   }
   ngOnInit(): void {
     console.log(this.contactForm.value);
+    AOS.init();
+    AOS.refresh();
   }
   Send() {
     console.log(this.contactForm.value);
   }
-
+  calculate_age(dob: Date) {
+    var diff_ms = Date.now() - dob.getTime();
+    var age_dt = new Date(diff_ms);
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
+  }
+  age = this.calculate_age(new Date(1999, 2, 19));
 }
